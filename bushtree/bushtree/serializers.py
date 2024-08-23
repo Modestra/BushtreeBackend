47477b90c9b79693f1dd8110c4e0d4fbb3edfd75
Modestra <umbrellamixmail@gmail.com>
2024-08-24 02:01:20 +0700

    Добавление тестового view

---
+++ bushtree/bushtree/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from bushtree.models import *
+
+class FlowerSerializer(serializers.ModelSerializer):
+    """Форма для запроса необходимого цветника"""
+    class Meta:
+        model = Flowers
+        field = ["name", "description", "frozen_resistance", "sunlight", "period_blossom_start", 
+                 "period_blossom_end", "height", "color_bloss_name", "color_bloss_hex", "color_leaves_name", "color_leaves_hex"]
--- bushtree/bushtree/urls.py
+++ bushtree/bushtree/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bushtree.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('getflower/', FLowerApiView.as_view(), name="getflower")
 ]
