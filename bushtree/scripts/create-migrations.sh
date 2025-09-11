7152b4ef9dac328cde7fe9756ae7746d7e98f5fc
Modestra <umbrellamixmail@gmail.com>
2025-09-11 18:52:13 +0700

    gitignore update

--- bushtree/scripts/create-migrations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/
-python manage.py migrate
-export DJANGO_SUPERUSER_USERNAME=Modestra
-export DJANGO_SUPERUSER_PASSWORD=Terrarik22
-export DJANGO_SUPERUSER_EMAIL="testuser@example.com"
-python manage.py createsuperuser --noinput
--- bushtree/scripts/create-users.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash/
-export DJANGO_SUPERUSER_USERNAME=Modestra
-export DJANGO_SUPERUSER_PASSWORD=Terrarik22
-export DJANGO_SUPERUSER_EMAIL="testuser@example.com"
-python manage.py createsuperuser --noinput
