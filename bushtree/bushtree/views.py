75eb2d1f2a0f53dd130d6258178e989dc40cd4de
Modestra <umbrellamixmail@gmail.com>
2025-09-14 00:30:09 +0700

    Обновить маршруты

--- bushtree/bushtree/views.py
+++ bushtree/bushtree/views.py
@@ -1,7 +1,5 @@
-from rest_framework import (status, viewsets)
-from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.parsers import MultiPartParser, JSONParser
-from rest_framework_simplejwt.tokens import RefreshToken
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.decorators import action
