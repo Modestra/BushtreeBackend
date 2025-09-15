0ac7de7b6d0fa49b200a8958049f038c446c49d3
Modestra <umbrellamixmail@gmail.com>
2025-09-16 00:26:43 +0700

    upd

--- bushtree/bushtree/settings.py
+++ bushtree/bushtree/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'bushtree.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'POST': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
